58ef435cdf6ebfacb0d2416988834dce8fda2f43
student <z@>
2025-03-29 10:32:17 +0300

    При введение правильного пароля и логина переводи на новое окно с другими функциями

---
+++ Driverrrrrrrrrr/Driverrrrrrrrrr/Views/DriverView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Driverrrrrrrrrr.Views;
+
+public partial class DriverView : UserControl
+{
+    public DriverView()
+    {
+        InitializeComponent();
+    }
+}
--- Driverrrrrrrrrr/Driverrrrrrrrrr/Views/MainView.axaml.cs
+++ Driverrrrrrrrrr/Driverrrrrrrrrr/Views/MainView.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Driverrrrrrrrrr.Classes;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Dto;
 
 namespace Driverrrrrrrrrr.Views;
 
@@ -7,5 +11,17 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (LoginTB.Text == "ispector" && PassTB.Text == "ispector")
+        {
+            Help.CCV.Content = new DriverView();
+        }
+        else
+        {
+            MessageBoxManager.GetMessageBoxStandard("EROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "Вы ввели не правельный логин, попробуйте - ispector").ShowAsync();
+        }
     }
 }
